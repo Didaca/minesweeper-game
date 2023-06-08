6a0f04ad1e27094e86e58fd6f04c496b15efd980
Didaca <deni_sk8@abv.bg>
2023-06-08 14:36:24 +0300

    Created the first screen with the numbers & bombs icons

--- main.py
+++ main.py
@@ -4,55 +4,56 @@ import random
 pygame.init()
 
 running = True
+rerun = True
 
-# screen parameters
-WIDTH = 400
-HEIGHT = 600
-SCREEN_COLOR = "#3C3F41"
+# Screen parameters
+WIDTH = 500
+HEIGHT = 500
+SCREEN_COLOR = "#D3D3D3"
+SCREEN_IMAGE = "bomb32.png"
 
-ROWS = 6
-COLUMNS = 6
-MINES = 4
+ROWS = 10
+COLUMNS = 10
+MINES = 10
 FIELD = []
 
-# cube parameters
-CUBE_COLOR = "#D3D3D3"
-CUBE_WIDTH = 0
-CUBE_RADIUS = 4
+# Cube parameters
+CUBE_COLOR = "#3C3F41"
+CUBE_SIZE = WIDTH // COLUMNS
+CUBE_RADIUS = 12
+CUBE_BOMB = "bomb.png"
+CUBE_NUMBERS_COLOR = {1: 'white', 2: 'green', 3: 'red', 4: 'blue',
+                      5: 'purple', 6: 'yellow', 7: 'lightblue', 8: 'pink'}
 
+# Game  GUI parameters
+logo = pygame.image.load(SCREEN_IMAGE)
+pygame.display.set_icon(logo)
 pygame.display.set_caption("Minesweeper")
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+FONT = pygame.font.SysFont("Helvetica", 20)
 
 
 def modify_cells(b_row, b_col):
 
     # row calc
 
-    if (b_row > 0) and (b_row < ROWS - 1):
+    if b_row > 0:
         if FIELD[b_row - 1][b_col] != -1:
             FIELD[b_row - 1][b_col] += 1
+    if b_row < ROWS - 1:
         if FIELD[b_row + 1][b_col] != -1:
             FIELD[b_row + 1][b_col] += 1
-    if b_row == 0:
-        if FIELD[b_row + 1][b_col] != -1:
-            FIELD[b_row + 1][b_col] += 1
-    if b_row == ROWS - 1:
-        if FIELD[b_row - 1][b_col] != -1:
-            FIELD[b_row - 1][b_col] += 1
 
     # col calc
 
-    if (b_col > 0) and (b_col < COLUMNS - 1):
+    if b_col > 0:
         if FIELD[b_row][b_col - 1] != -1:
             FIELD[b_row][b_col - 1] += 1
+    if b_col < COLUMNS - 1:
         if FIELD[b_row][b_col + 1] != -1:
             FIELD[b_row][b_col + 1] += 1
-    if b_col == 0:
-        if FIELD[b_row][b_col + 1] != -1:
-            FIELD[b_row][b_col + 1] += 1
-    if b_col == COLUMNS - 1:
-        if FIELD[b_row][b_col - 1] != -1:
-            FIELD[b_row][b_col - 1] += 1
 
     # diagonals
 
@@ -132,7 +133,42 @@ def create_field(rows, cols, mines):
         modify_cells(r, c)
 
 
+def create_screen():
+
+    for ind1, r in enumerate(FIELD):
+        for ind2, c in enumerate(r):
+            x = ind1 * CUBE_SIZE
+            y = ind2 * CUBE_SIZE
+
+            pygame.draw.rect(screen, CUBE_COLOR, (x, y, CUBE_SIZE, CUBE_SIZE), border_radius=CUBE_RADIUS)
+            pygame.draw.rect(screen, SCREEN_COLOR, (x, y, CUBE_SIZE, CUBE_SIZE), 1, border_radius=CUBE_RADIUS)
+
+            if c < 0:
+                bomb_in_cube = pygame.image.load(CUBE_BOMB)
+                x_numInCube = bomb_in_cube.get_width()
+                y_numInCube = bomb_in_cube.get_height()
+                screen.blit(
+                    bomb_in_cube,
+                    (x + (CUBE_SIZE // 2 - x_numInCube / 2), y + (CUBE_SIZE // 2 - y_numInCube / 2))
+                )
+
+            if c > 0:
+                number_in_cube = FONT.render(str(c), True, CUBE_NUMBERS_COLOR[c])
+                x_numInCube = number_in_cube.get_width()
+                y_numInCube = number_in_cube.get_height()
+                screen.blit(
+                    number_in_cube,
+                    (x + (CUBE_SIZE // 2 - x_numInCube / 2), y + (CUBE_SIZE // 2 - y_numInCube / 2))
+                )
+
+    pygame.display.update()
+
+
 while running:
+
+    if rerun:
+        create_field(ROWS, COLUMNS, MINES)
+        rerun = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -141,16 +177,9 @@ while running:
 
     screen.fill(SCREEN_COLOR)
 
-    create_field(ROWS, COLUMNS, MINES)
-
-
+    create_screen()
     # RENDER YOUR GAME HERE
 
     pygame.display.flip()
 
-    pygame.quit()
-
 pygame.quit()
-
-# pygame.draw.rect(screen, CUBE_COLOR, ((r, c), (30, 30)), width=CUBE_WIDTH, border_radius=CUBE_RADIUS)
-# pygame.display.update()
